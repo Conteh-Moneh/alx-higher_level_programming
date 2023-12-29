b580e64f25816bd8ae9d9afb403f87eea9aa984a
Conteh-Moneh <contehthompsonmoneh@icloud.com>
2023-12-29 09:19:38 -0800

    Python program that prints the last digit of given random numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,3 +8,4 @@ elif number == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif number < 6 and number != 0:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+print(" ")
