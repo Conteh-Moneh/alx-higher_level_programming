d263510c811949923691ae642b71db82d92668ec
Conteh-Moneh <contehthompsonmoneh@icloud.com>
2024-03-28 08:24:49 -0700

    A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 #Function to send GET request to URL and display body of response
-get_response_body(){
-	url=$1
-	response=$(curl -s -o /dev/null -w "%{http_code}" $url)
-	if [$response -eg 200]; then
-		curl -s $url
-	else
-		echo "Error: Non-200 status code received"
-	fi
-}
-#Main script
-read -p "Enter the URL: " input_url
-get_response_body $input_url
-
+get_response_body(){ url=$1; response=$(curl -s -o /dev/null -w "%{http_code}" $url); [ $response -eq 200 ] && curl -s $url || echo "Error: Non-200 status code received"; }; read -p "Enter the URL: " input_url; get_response_body $input_url
